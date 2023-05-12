b39212a2256ce57063daa6fff5db6035146b02a8
Umberto Colangelo <umberto.colangelo@mail.polimi.it>
2023-05-12 16:21:24 +0200

    modified:   .DS_Store 	new file:   olio_evo/lib/fonts/SF-Pro.ttf 	modified:   olio_evo/lib/widgets/widget_home_products.dart 	modified:   olio_evo/pubspec.yaml

--- olio_evo/lib/widgets/widget_home_products.dart
+++ olio_evo/lib/widgets/widget_home_products.dart
@@ -4,6 +4,7 @@ import 'package:olio_evo/api_service.dart';
 import 'package:intl/intl.dart';
 import '../models/product.dart';
 
+
 class WidgetHomeProducts extends StatefulWidget {
   String labelName;
   String tagId;
@@ -29,17 +30,18 @@ class _WidgetHomeProductState extends State<WidgetHomeProducts> {
       
       decoration: BoxDecoration(
         color: Color.fromARGB(255, 249, 249, 249),
-        border: Border.all(
-                         width: 1,
-                          // assign the color to the border color
-                          color: Color.fromARGB(255, 12, 12, 12),
-      ),),
-      child: Column(children: <Widget>[
+        
+        ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget> [
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
             Padding(
-              padding: EdgeInsets.only(left: 16, top: 4),
+              padding: EdgeInsets.all(10),
               child: Text(
                 this.widget.labelName,
                 style: TextStyle(
@@ -50,7 +52,7 @@ class _WidgetHomeProductState extends State<WidgetHomeProducts> {
               ),
             ),
             Padding(
-              padding: EdgeInsets.only(left: 16, top: 4),
+              padding: EdgeInsets.all(10),
               child: TextButton(
                 onPressed: () {},
                 child: Text(
@@ -80,61 +82,75 @@ class _WidgetHomeProductState extends State<WidgetHomeProducts> {
 
   Widget _buildList(List<Product> items) {
     return Container(
-      height: 320,
-      alignment: Alignment.centerLeft,
-      child: ListView.builder(
-        shrinkWrap: true,
-        physics: ClampingScrollPhysics(),
-        scrollDirection: Axis.horizontal,
+      height: MediaQuery.of(context).size.height / 1.7, //altezza box
+      alignment: Alignment.center,
+      child: GridView.builder(
+        physics: NeverScrollableScrollPhysics(),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 0.8,
+        ),
         itemCount: items.length,
         itemBuilder: (context, index) {
           var data = items[index];
-
           return Container(
-            padding: EdgeInsets.all(5),
-            margin: EdgeInsets.all(10),
-            width: 180,
-            height: 200,
             alignment: Alignment.center,
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(0),
+            width: MediaQuery.of(context).size.width / 2.2,
+            height: MediaQuery.of(context).size.height / 0.5,
             decoration: BoxDecoration(
+              color: Color(0xffffffff),
               shape: BoxShape.rectangle,
-              borderRadius: BorderRadius.circular(4),
-              color: Colors.white,
+              borderRadius: BorderRadius.circular(10.0),
               boxShadow: [
                 BoxShadow(
                   color: Colors.black12,
-                  offset: Offset(0, 5),
-                  blurRadius: 15,
+                  offset: Offset(0, 0),
+                  blurRadius: 3,
                 )
               ],
-            ),
+              ),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
               children: <Widget>[
                 Row(
                   mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisSize: MainAxisSize.max,
                   children: [
+                    /*
                     Padding(
-                      padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
+                      padding: EdgeInsets.fromLTRB(0, 0, 30, 0),
+                      child: IconButton(
+                        icon: Icon(Icons.favorite_border_outlined),
+                        onPressed: () {},
+                        color: Color(0xff212435),
+                        iconSize: 30,
+                      ),
+                    ),
+                    */
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(10, 20, 3, 0),
                       child: Text(
                           data.rating.toString()== '0'
-                        ? '3'
+                        ? '3.0'
                         : 'data.rating.toString()',
                         textAlign: TextAlign.left,
                         overflow: TextOverflow.clip,
                         style: TextStyle(
-                          fontWeight: FontWeight.w500,
-                          fontStyle: FontStyle.normal,
-                          fontSize: 18,
-                          color: Color(0xff000000),
+                          fontFamily: 'SFPro',
+                          fontWeight: FontWeight.w400,
+                          //fontStyle: FontStyle.italic,
+                          fontSize: 20,
+                          color: Color.fromARGB(255, 94, 94, 94),
                         ),
                       ),
                     ),
                     Padding(
-                      padding: EdgeInsets.fromLTRB(5, 0, 0, 10),
+                      padding: EdgeInsets.fromLTRB(0, 21, 0, 0),
                       child: RatingBar.builder(
                         initialRating:   
                         data.rating.toDouble()==0
@@ -144,35 +160,37 @@ class _WidgetHomeProductState extends State<WidgetHomeProducts> {
                         itemBuilder: (context, index) =>
                             Icon(Icons.star, color: Color(0xffffc107)),
                         itemCount: 5, //numero stelline
-                        itemSize: 15, //dimensione stelline
+                        itemSize: 20, //dimensione stelline
                         direction: Axis.horizontal,
                         allowHalfRating: true,
                         onRatingUpdate: (value) {},
                       ),
                     ),
                     Padding(
-                      padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                      padding: EdgeInsets.fromLTRB(0, 10, 0, 5),
                       child: IconButton(
-                        icon: Icon(Icons.add_circle),
+                        icon: Icon(Icons.add_shopping_cart_outlined),
                         onPressed: () {},
                         color: Color(0xff212435),
-                        iconSize: 24,
+                        iconSize: 35,
                       ),
                     ),
                   ],
                 ),
                 Image.network(
                   data.images[0].src,
-                  height: 170,
+                  height: 100,
                 ),
                 Container(
                   margin: EdgeInsets.only(
-                      top: 4, left: 4), //qui il nome del prodotto
+                      top: 15, left: 4), //qui il nome del prodotto
                   width: 180,
                   alignment: Alignment.center,
                   child: Text(data.name,
                       textAlign: TextAlign.center,
                       style: TextStyle(
+                        fontFamily: 'SFPro',
+                        fontWeight: FontWeight.w500,
                         fontSize: 14,
                         color: Colors.black,
                       )),
@@ -180,25 +198,44 @@ class _WidgetHomeProductState extends State<WidgetHomeProducts> {
                 Container(
                   // qui il prezzo
                   margin: EdgeInsets.only(top: 4, left: 4),
-                  width: 170,
+                  width: 150,
                   alignment: Alignment.center,
                   child: Text(
                     data.salePrice != null && data.salePrice.isNotEmpty
-                        ? 'In offerta: ${data.salePrice} € '
-                        : '${data.regularPrice} €',
+                        ? 'In offerta: € ${data.salePrice}'
+                        : '€ ${data.regularPrice}',
                     textAlign: TextAlign.center,
                     style: TextStyle(
-                      fontSize: 14,
+                      fontFamily: 'SFPro',
+                      fontSize: 18,
                       color: data.salePrice != null && data.salePrice.isNotEmpty
                           ? Colors.red
                           : Colors.black,
                       fontWeight:
                           data.salePrice != null && data.salePrice.isNotEmpty
-                              ? FontWeight.bold
-                              : FontWeight.bold,
+                              ? FontWeight.w800
+                              : FontWeight.w800,
                     ),
                   ),
                 ),
+                /* riga con icone sapori
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                      child: IconButton(
+                        icon: Icon(Icons.workspace_premium),
+                        onPressed: () {},
+                        color: Color(0xff212435),
+                        iconSize: 1,
+                      ),
+                    ),
+                  ],
+                ),
+                */
               ],
             ),
           );
